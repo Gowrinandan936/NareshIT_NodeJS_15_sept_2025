42d8886575ace4366de0c88f7ece12ef8fdb8fec
Nandan <nandanthangella24@gmail.com>
2025-10-04 02:46:57 +0530

    commented semicolon

--- day_14_sept_30/addition_with_arg.js
+++ day_14_sept_30/addition_with_arg.js
@@ -3,4 +3,4 @@ let [a,b] = process.argv.slice(2);
 if( a && b)
     console.log(`Addition of ${a} and ${b} is ${+a + +b}`);
 else
-    console.log('Please pass 2 params')
+    console.log('Please pass 2 params');
